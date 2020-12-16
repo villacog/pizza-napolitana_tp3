669d215498753302f7e8a3747c059f8c695da299
villacog <74794276+villacog@users.noreply.github.com>
2020-12-16 11:59:12 -0300

    Add files via upload

---
+++ js/adiv1.js
@@ -0,0 +1,55 @@
+const entrada = document.getElementById('entrada');
+const seccion = document.getElementById('seccion');
+const vidas = document.getElementById('vidas');
+var intentos = 4;
+const solucion = 'italia' 
+const vis1 = 'hidden';
+const vis2 = 'visible';
+rows = ['row1', 'row2', 'row3', 'row4']
+var i = 0;
+
+
+function gano() {
+    vidas.innerHTML =  'Gano! La respuesta correcta es italia. ¡Felicitaciones!' ;
+    seccion.style.visibility = vis1;
+
+};
+
+function perdio() { 
+    vidas.innerHTML = 'Perdio. La respuesta correcta es italia';
+    
+    seccion.style.visibility = vis1
+
+};
+
+
+function intento() {
+
+    event.preventDefault();
+    console.log(entrada.value);
+    
+    if (entrada.value == solucion) {
+        gano();
+
+    } else if (intentos > 1) {
+        i = i +1
+        intentos = intentos - 1
+
+        vidas.innerHTML =  'Intentos ' + intentos;
+        document.getElementById(rows[i]).style.visibility = vis2;
+
+
+    }
+    else {
+        vidas.innerHTML =  'Intentos ' + intentos;
+        perdio();
+    }
+
+  }
+
+  
+
+
+
+
+
